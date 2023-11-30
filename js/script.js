4815cb65f12eda30d8a40d2085d3c3330487dc75
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-30 16:00:10 +0100

    Created function that generates random number

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+// Punto 3 readme.md
+// Funzione in cui determiniamo un array vuoto
+function createBombs(){
+    let array = [];
+
+    // Inserisco all'interno dell'array i numeri che ho generato casualmente
+    for(let i=0; i<100; i++){
+        array.push(createRandomNumbers(array));
+    }
+    
+    return array;
+}
+
+// Punto 4 readme.md
+// Funzione che mi genera numeri random
+function createRandomNumbers(arrayNumbers){
+    let flag = false;
+    let number;
+    
+    while(flag == false){
+        number = Math.floor(Math.random() * 100 + 1);
+
+        if(arrayNumbers.includes(number) == false){
+            flag = true;
+        }
+    }
+   
+    return number;
+}
+
 // Funzione che mi crea la singola cella della griglia
 function createCells(num){
 
